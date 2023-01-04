a400903b7a95e129e29f854dd3eb82e8818758e3
prawar <prawarmundra20@gmail.com>
2023-01-04 20:05:19 +0530

    added new deploy url

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000/api/"
+const BASE_URL = "https://mundras-backend.onrender.com/api/"
 // const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyODBiZWRmYTQ1MjMwYTRiMWViOWE3NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MzU4ODQ2OCwiZXhwIjoxNjUzODQ3NjY4fQ.ZE9XhHq-rGu2ojoKAep3HwHdG7QvEUQLkOAEN4pjzO4";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
